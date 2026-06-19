f40072d8ea566f8b06ff5236f98e95d221a1e678
Gabriel <muehlbauergabriel@gmail.com>
2026-06-18 22:15:43 -0300

    Evoluções no design do componente de detalhes das despesas

--- expenses-control-front-end/src/components/ExpenseDetails/ExpenseDetails.jsx
+++ expenses-control-front-end/src/components/ExpenseDetails/ExpenseDetails.jsx
@@ -5,18 +5,18 @@ function ExpenseDetails({ expense, onClose }) {
   // Proteção: Se por algum motivo o componente abrir sem uma despesa, não deixa o app crashar
   if (!expense) return (
     <>
-    <div className={styles.loadingDisplay}>
+      <div className={styles.loadingDisplay}>
         <div className={styles.loadingContent}>
-            <img src={loading} alt="loading" />
-            Carregando...
+          <img src={loading} alt="loading" />
+          Carregando...
         </div>
-    </div>
+      </div>
     </>
   );
 
   return (
     <div className={styles.container}>
-      
+
       {/* Cabeçalho com o botão de fechar e a Categoria estilo "tag" */}
       <div className={styles.header}>
         <h2 className={styles.title}>{expense.title}</h2>
@@ -32,15 +32,19 @@ function ExpenseDetails({ expense, onClose }) {
         <p className={styles.amountLabel}>Valor da Despesa</p>
         <h3 className={styles.amountValue}>R$ {expense.amount}</h3>
       </div>
-    
-    <div className={styles.categoryTag}>
+
+      <div className={styles.categoryTag}>
         <h4>{expense.category}</h4>
-    </div>
+      </div>
 
       {/* Botões de Ação para o CRUD completo (Editar e Eliminar) */}
       <div className={styles.actions}>
-        <button className={styles.editButton}>Editar despesa</button>
-        <button className={styles.deleteButton}>Excluir despesa</button>
+        <button className={styles.editButton}>
+          <span class="material-symbols-outlined">edit</span>
+        </button>
+        <button className={styles.deleteButton}>
+          <span class="material-symbols-outlined">delete</span>
+        </button>
       </div>
 
     </div>
